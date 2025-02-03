453839dff005d67a8bd4bbdd0bd190b78bbb7491
NathanaelKhor <nwxkhor@gmail.com>
2025-02-03 20:00:28 +1100

    API's

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,45 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { connectDB } = require("./config/db");
+const Product = require("./models/product");
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => {
-    console.log('Server started at http://localhost:5000')
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in Create Product", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {}
+});
+
+app.listen(8080, async () => {
+  await connectDB();
+  console.log("Server started at http://localhost:8080");
 });
