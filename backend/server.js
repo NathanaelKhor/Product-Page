95afdd1e76673f8cd9c02f1c25cd761ea878349e
NathanaelKhor <nwxkhor@gmail.com>
2025-02-04 18:52:52 +1100

    Backend API donse

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,13 @@ const productRoutes = require("./routes/productRouter");
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-app.listen(8080, async () => {
+app.listen(PORT, async () => {
   await connectDB();
   console.log("Server started at http://localhost:8080");
 });
